25d354d4cb0d2d10c624bb4e4d62fb8a019de3f3
roma-dev-expert <roma.k.devexpert@gmail.com>
2025-04-07 15:33:17 +0300

    Initial commit: Created a basic ASP.NET Core Web API template for logging requests and responses

---
+++ Program.cs
@@ -0,0 +1,15 @@
+var builder = WebApplication.CreateBuilder(args);
+
+// Add services to the container
+builder.Services.AddDbContext<AppDbContext>(options =>
+    options.UseSqlServer(builder.Configuration.GetConnectionString("DefaultConnection")));
+builder.Services.AddControllers();
+
+var app = builder.Build();
+
+// Configure the HTTP request pipeline
+app.UseHttpsRedirection();
+
+app.MapControllers();
+
+app.Run();
